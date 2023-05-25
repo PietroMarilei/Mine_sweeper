23a2f7e9d9743049eeb576fc0c2929ec3bfbea88
PietroMarilei~ <pietromrl0@gmail.com>
2023-05-25 11:35:34 +0200

    add flag toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,13 @@ function startGame() {
     //This is the 2d array creator. Create a row, then add the first col with a single cell, then goes to the second col and so on. Then create the new row etc. Assigning every tile an Id "row-col" to indentify that. 
 
     for (let r = 0; r < rows; r++) {
+        // Creatthe 1 wmpty row 
         let row = []
+        //starting from 0 col populate the row with col containing sigle tile
         for (let c = 0; c < columns; c++) {
             let tile = document.createElement('div');
             tile.id = r.toString() + "-" + c.toString()
+            tile.addEventListener('click', clickTile)
             // add to dom
             document.getElementById("board").append(tile);
             // add the tile to the row array
@@ -33,9 +36,25 @@ function startGame() {
         }
 
         board.push(row);
-        console.log(board);
     }
 
+    console.log(board);
 
 
+}
+
+function clickTile() {
+    //this si riferisce all'elemento cliccato che scatena l'evento, appunto tile
+    let tile = this
+    if (flagEnable)
+    // toggle flag function
+    {
+        if (tile.innerText == "") {
+            tile.innerText = "🚩"
+        } else if (tile.innerText == "🚩") {
+            tile.innerText == ""
+
+
+        }
+    }
 }
