6981d78527a73c99717eec507715beb421b6669a
PietroMarilei~ <pietromrl0@gmail.com>
2023-05-27 17:43:07 +0200

    add flag function add manual mines array

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,16 @@ window.onload = function () {
     startGame()
 }
 
+function setMines() {
+    minesLocation.push("2-2")
+    minesLocation.push("2-3")
+    minesLocation.push("1-1")
+}
+
 function startGame() {
-    document.getElementById('mines_count').innerText = minesCount;
-
+    document.getElementById('mines_count').innerText = minesLocation;
+    document.getElementById('flag_button').addEventListener('click', setFlag)
     //This is the 2d array creator. Create a row, then add the first col with a single cell, then goes to the second col and so on. Then create the new row etc. Assigning every tile an Id "row-col" to indentify that. 
-
     for (let r = 0; r < rows; r++) {
         // Creatthe 1 wmpty row 
         let row = []
@@ -37,24 +42,44 @@ function startGame() {
 
         board.push(row);
     }
-
     console.log(board);
+    setMines()
+    console.log(minesLocation);
+}
 
+function setFlag() {
+    if (flagEnable) {
+        flagEnable = false;
+        document.getElementById("flag_button").style.backgroundColor = "lightgrey"
 
+    } else {
+        flagEnable = true;
+        document.getElementById("flag_button").style.backgroundColor = "darkgrey"
+    }
 }
 
 function clickTile() {
     //this si riferisce all'elemento cliccato che scatena l'evento, appunto tile
     let tile = this
-    if (flagEnable)
-    // toggle flag function
-    {
+    if (flagEnable) {
+        // toggle flag function is true the replace text
         if (tile.innerText == "") {
             tile.innerText = "🚩"
+
         } else if (tile.innerText == "🚩") {
-            tile.innerText == ""
-
-
+            tile.innerText = ""
         }
+        return;
     }
+
+    if (minesLocation.includes(tile.id)) {
+        alert("GAME OVER");
+        gameOver = true;
+        revealMines();
+        return;
+    }
+}
+
+function revealMines() {
+
 }
