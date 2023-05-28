39bb600800124b142bea6ad773b3374838ea2ff7
PietroMarilei~ <pietromrl0@gmail.com>
2023-05-28 11:00:40 +0200

    random errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,7 @@ function setMines() {
 function startGame() {
     document.getElementById('mines_count').innerText = minesLocation;
     document.getElementById('flag_button').addEventListener('click', setFlag)
+
     //This is the 2d array creator. Create a row, then add the first col with a single cell, then goes to the second col and so on. Then create the new row etc. Assigning every tile an Id "row-col" to indentify that. 
     for (let r = 0; r < rows; r++) {
         // Creatthe 1 wmpty row 
@@ -39,9 +40,9 @@ function startGame() {
             // add the tile to the row array
             row.push(tile)
         }
-
         board.push(row);
     }
+
     console.log(board);
     setMines()
     console.log(minesLocation);
@@ -73,13 +74,64 @@ function clickTile() {
     }
 
     if (minesLocation.includes(tile.id)) {
-        alert("GAME OVER");
         gameOver = true;
         revealMines();
+        //alert("GAME OVER");
         return;
     }
+
+    let coords = tile.id.split("-") // "1-1"=> ["1","1"] queste sono le coord di un singolo tile ottenute dal suo id. 
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+    checkMine()
 }
 
 function revealMines() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = board[r][c];
+            if (minesLocation.includes(tile.id)) {
+                tile.innerText = "💣"
+                tile.style.backgroundColor = "red"
+            }
 
+        }
+    }
+}
+
+function checkMine(r, c) {
+    if (r < 0 || r >= rows || c < 0 || c >= columns)
+        return;
+    // no mines out of buond
+    let minesFound = 0;
+
+    //check top 3 tiles, dice solo quante mine ha trovato vicine sopra
+    minesFound += checkTile(r - 1, c - 1)   //top left
+    minesFound += checkTile(r - 1, c)      //top
+    minesFound += checkTile(r - 1, c + 1)   //top right
+
+    //check left right tiles
+    minesFound += checkTile(r, c - 1)       //left
+    minesFound += checkTile(r, c + 1)      //right
+
+    //check bottom 3 tiles
+    minesFound += checkTile(r + 1, c - 1)   //bot left
+    minesFound += checkTile(r + 1, c)       //bot
+    minesFound += checkTile(r + 1, c + 1)   //bot right
+
+    if (minesFound > 0) {
+        board[r, c].innerText = minesFound
+        board[r, c].classList.add("x" + minesFound.toString())
+    }
+}
+
+function checkTile(r, c) {
+    if (r < 0 || r >= rows || c < 0 || c >= columns)
+        return 0;
+
+    // 🔽 se tra le minesLocation c'è una stringa che corrisponde alla tile controllata r-c allora return 1
+    if (minesLocation.includes(r.toString() + "-" + c.toString())) {
+        return 1;
+    }
+    return 0;
 }
